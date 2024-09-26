9c6dd9c89960b88639ef6069ab3b3d73025651ba
Robert Hogan <rhogan@cergenx.com>
2024-09-26 10:23:51 +0100

    install dependencies

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='speed',
-    version='0.1',
+    version='0.1.2',
     packages=find_packages(include=['speed', 'speed.*']),
+    install_requires=[
+        'numpy>=1.24.3',
+        'scipy>=1.10.1',
+        'statsmodels>=0.14.1',
+        'pandas>=2.0.1',
+        'matplotlib>=3.7.1',
+        'seaborn>=0.12.2',
+        'scikit-learn>=1.2.2',
+        'pytest>=7.3.1',
+    ],
 )
